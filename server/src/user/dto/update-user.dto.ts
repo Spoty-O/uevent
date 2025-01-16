c43c0749d7301b40aa89338baba34b70cf1d6466
Spoty-O <korotysh16@ukr.net>
2025-01-16 23:08:50 +0200

    Updated uset table and user-update class

--- server/src/user/dto/update-user.dto.ts
+++ server/src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
+  picturePath?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  visible?: boolean;
+}
--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -5,12 +5,24 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true, nullable: false })
+  username: string;
+
+  @Column({ unique: true, nullable: false })
   email: string;
 
-  @Column()
+  @Column({ nullable: false })
   password: string;
 
-  @Column()
-  username: string;
+  @Column({ enum: ['user', 'admin'], default: 'user' })
+  role: 'user' | 'admin';
+
+  @Column({ default: 'default.jpg' })
+  picturePath: string;
+
+  @Column({ default: false })
+  confirmed: boolean;
+
+  @Column({ default: true })
+  visible: boolean;
 }
