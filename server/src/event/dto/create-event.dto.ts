e0f4544a150d960153fe95a0b94a42714b0286ca
unknown <jeka20001980@gmail.com>
2025-01-30 03:24:43 +0100

    add create event dto

--- server/src/event/dto/create-event.dto.ts
+++ server/src/event/dto/create-event.dto.ts
@@ -1,1 +1,10 @@
-export class CreateEventDto {}
+import { IsDate, IsString, MinDate } from 'class-validator';
+
+export class CreateEventDto {
+  @IsString()
+  title: string;
+
+  @IsDate()
+  @MinDate(new Date())
+  date: Date;
+}
