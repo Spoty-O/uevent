cbd77848c58f926fe5167acb5153383a3137cd78
Spoty-O <korotysh16@ukr.net>
2025-02-02 19:57:17 +0200

    Fixed user entity
    * Updated dto, added more validation options
    * Updated user controller, because validation pipe changed to global

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,12 +1,19 @@
-import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
+  @MaxLength(255)
   email: string;
 
   @IsStrongPassword({ minLength: 6 })
   password: string;
 
   @IsString()
+  @MaxLength(50)
   username: string;
 }
