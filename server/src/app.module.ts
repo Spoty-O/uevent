f9438345c8a3c814071d029c27b055c361868582
Spoty-O <100100554+Spoty-O@users.noreply.github.com>
2025-02-02 19:46:45 +0200

    Revert "Feature/company entity"

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { validate } from './env.validation';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfigService } from './db.config';
-import { UserModule } from './user/user.module';
-import { CompanyModule } from './company/company.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-      validate,
-    }),
-    TypeOrmModule.forRootAsync({
-      useClass: TypeOrmConfigService,
-    }),
-    UserModule,
-    CompanyModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  const config = app.get(ConfigService);
-  const PORT = config.get('PORT') || 5000;
-  await app.listen(PORT || 5000, () =>
-    console.log(`Server started on port:${PORT}`),
-  );
+  await app.listen(3000);
 }
 bootstrap();
